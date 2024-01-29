f10f57e5e9939f60c6b31be6b75edadf461c88c8
DcoderLLC <dcoderllc@gmail.com>
2024-01-29 14:57:13 +0530

    Hero page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero/>
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
-import HiLocationMarker from 'react-icons/hi'
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from 'react-countup'
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -16,16 +17,38 @@ const Hero = () => {
             </h1>
           </div>
           <div className=" flexColStart hero-des">
-            <span>Find a Variety of Properties that suit you very easily</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">Find a Variety of Properties that suit you very easily</span>
+            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
           </div>
 
-          <div className="search-bar">
+          <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
           </div>
-          
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={0} end={20} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Projects</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp  end={50} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp  end={5} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Patents</span>
+            </div>
+          </div>
         </div>
         {/* Right Side */}
         <div className="flexCenter hero-right">

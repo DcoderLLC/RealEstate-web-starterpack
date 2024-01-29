d9ca15023ed62ca82346cfe87ece40c7b1372883
DcoderLLC <dcoderllc@gmail.com>
2024-01-29 15:11:05 +0530

    Companies section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/companies/Companies";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Companies />
     </div>
   );
 }
---
+++ src/components/Residency/Residency.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import './Residency.css'
+import 'Swiper/css'
+import data from '../../utils/slider.json'
+const Residency = () => {
+  return (
+    <div>Residency</div>
+  )
+}
+
+export default Residency
